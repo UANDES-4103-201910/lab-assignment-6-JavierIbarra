ab29ad398869b00d13ba36f03a7d87958f69e876
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-20 10:08:40 -0400

    Completed create method (Sessions controller)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
 
 	def create
 		#complete this method
+		user = User.where(email: params[:session][:email], password: params[:session][:password]).first
+		if user.nil? == false
+			session[:current_user_id] = params[:authenticity_token]
+			cookies["authentification"] = params[:authenticity_token]
+			flash[:notice] = "You are successfully log in"
+			redirect_to users_path
+		else
+			flash[:notice] = "Your email or password are incorrect"
+			redirect_to root_url
+		end
 	end
 
 	def destroy
