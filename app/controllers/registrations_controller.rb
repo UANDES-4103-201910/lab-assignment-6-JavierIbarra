60a51e742b96b960704250054437813b55e2cf29
JavierIbarra <jaibarra1@miuandes.cl>
2019-04-20 09:47:58 -0400

    Create method (Registration controller)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,14 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
-	    #complete this method
+		#complete this method
+		@user=User.create(name: params[:registrations][:name], last_name: params[:registrations][:last_name], email: params[:registrations][:email], phone: params[:registrations][:phone], password: params[:registrations][:password])
+		if @user.save
+			flash[:notice] = "your registration is complete"
+			redirect_to root_path
+		else
+			flash[:notice] = "Your registration has failed, try again"
+			redirect_to sign_in_path
+		end
 	end
 end
